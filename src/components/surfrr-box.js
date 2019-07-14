6d3b1f7fc82a66d01c100728de1a6571190895aa
Kevin Cartwright <kevincart@gmail.com>
2019-07-14 18:13:53 +1200

    merge in stuff from old project

---
+++ src/components/surfrr-box.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SurfrrBox extends React.Component {
+    render() {
+      return (
+        <div className="box-root"></div>
+      );
+    }
+  }
+
+  export default SurfrrBox;
