a6ac567106746c4c8b6ff37fd516a7daa2306b1c
Kevin Cartwright <kevincart@gmail.com>
2019-07-14 14:49:55 +1200

    edit app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Surfr
+      </p>
+        <span role="img" aria-label="Surfr">
+          🏄
+        </span>
       </header>
       <body>
-        <span role="img" aria-label="Surfr">
-        🏄
-        </span>
       </body>
     </div>
   );
