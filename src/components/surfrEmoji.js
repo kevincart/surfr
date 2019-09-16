ea679165f3893f3d44f54a7752077d739d9b928c
Kevin Cartwright <kevincart@gmail.com>
2019-09-16 16:46:10 +1200

    tidy up emoji stuff

--- src/components/surfrEmoji.js
+++ src/components/surfrEmoji.js
@@ -5,10 +5,12 @@ class Emoji extends React.Component {
         super(props);
         this.children = this.props.children;
         console.log('link props: ', this.props);
+
+        this.content = this.props.content;
     }
     render() {
       return (
-        <span role="img" aria-label="surfr-emoji" className="surfr-emoji"></span>
+        <span role="img" aria-label="surfr-emoji" className="surfr-emoji">{ "&#" + this.props.content + ";"}</span>
         // <a className="surfr-link" href="homepage.html">
         //     { this.props.children }
         // </a>
