19e6816e93956ed29439f4181521617fa2d09a63
Kevin Cartwright <kevincart@gmail.com>
2019-09-17 12:58:52 +1200

    change copy in about, make App-article selector

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,15 +2,22 @@
 import React from 'react';
 
 // Surfr Components
+import Box from '../components/surfrBox';
 import Nav from '../components/surfrNav';
 
 function About() {
     return (
         <div className="App">
-          <header className="App-header">
+            <header className="App-header">
             <Nav></Nav>
-          </header>
-          <div>This appears to be the about page</div>
+            </header>
+            <Box>
+                <article className="App-article">
+                    Surfr is a mobile-first design system made in React.
+                    You should feel free to fork it and use it 
+                    as a starter project for learning about design systems.
+                </article>
+            </Box>
         </div>
     );
 }
