e0b74a0e39179996433b1c6607b19e376f449605
Kevin Cartwright <kevin.cartwright@trademe.co.nz>
2022-11-11 14:17:33 +1300

    fix

--- src/index.js
+++ src/index.js
@@ -5,15 +5,23 @@ import Emoji from './components/surfrEmoji';
 import Nav from './components/surfrNav';
 import SVG from './components/surfrSVG';
 
+// React imports
+import * as ReactDOM from 'react-dom';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+// Internal imports
+import App from './App';
+
 // Style imports
 import './surfr.css';
 import './_reset.scss';
 
-// ReactDOM.render(
-//     <BrowserRouter basename="/surfr">
-//         <App />
-//     </BrowserRouter>,
-//     document.getElementById('root')
-//  );
+ReactDOM.render(
+    <BrowserRouter basename="/">
+        <App />
+    </BrowserRouter>,
+    document.getElementById('root')
+ );
 
 export default { Card, Box, Emoji, Nav, SVG };
